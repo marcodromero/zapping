ba019495eb214c0161f3fef6cf4fd09406140eaa
Marco Romero <marcod.romero@gmail.com>
2025-05-01 14:54:58 -0300

    Mejoras en la legibilidad del codigo

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-async function getFileM3U(url) {
-	const dataM3U = await fetch(url);
-	return await dataM3U.text();
+async function fetchM3UFile(url) {
+	const response = await fetch(url);
+	return await response.text();
 }
 
-function getChannels(textM3U) {
-	let arrayLinesOfText = textM3U.split('\n');
+function getChannels(M3UTextContent) {
+	let M3ULines = M3UTextContent.split('\n');
 	let channels = [];
-	let actualChannelData = null;
+	let currentChannel = null;
 	const regexMetadata =
 		/#EXTINF:(-?\d+)\s+tvg-id="([^"]*)"\s+tvg-logo="([^"]*)"\s+group-title="([^"]*)",(.*)/;
 	const regexURL = /(https?:\/\/[^\s]+)/g;
 
-	arrayLinesOfText.forEach((line) => {
-		const channelData = line.match(regexMetadata);
-		let channelURL = line.match(regexURL);
+	M3ULines.forEach((line) => {
+		const metadataMatch = line.match(regexMetadata);
+		let urlMatch = line.match(regexURL);
 
-		if (channelData) {
-			actualChannelData = {
-				duration: parseInt(channelData[1]),
-				tvgId: channelData[2],
-				tvgLogo: channelData[3],
-				group: channelData[4],
-				name: channelData[5].trim(),
+		if (metadataMatch) {
+			currentChannel = {
+				duration: parseInt(metadataMatch[1]),
+				tvgId: metadataMatch[2],
+				tvgLogo: metadataMatch[3],
+				group: metadataMatch[4],
+				name: metadataMatch[5].trim(),
 			};
-		} else if (actualChannelData && channelURL) {
+		} else if (currentChannel && urlMatch) {
 			channels.push({
-				...actualChannelData,
-				url: channelURL[0],
+				...currentChannel,
+				url: urlMatch[0],
 			});
 
-			actualChannelData = null;
+			currentChannel = null;
 		}
 	});
 
@@ -40,67 +40,73 @@ const getFavoritesChannels = () => {
 	return JSON.parse(localStorage.getItem('favoriteChannels'));
 };
 
-const buildChannelSection = (channel) => {
-	const channelSection = document.createElement('div');
-	channelSection.classList.add('flex');
-	channelSection.classList.add('w-full');
-	channelSection.classList.add('min-h-1/7');
-	channelSection.classList.add('border-t-1');
-	channelSection.classList.add('border-t-[#29374d]');
-	channelSection.classList.add('border-b-2');
-	channelSection.classList.add('border-b-[#0e121a]');
-	channelSection.classList.add('bg-[#1c2534]');
-	channelSection.classList.add('hover:bg-[#3a6280]');
-	channelSection.classList.add('channel');
+const buildChannelContainer = () => {
+	const channelContainer = document.createElement('div');
+	channelContainer.classList.add('flex');
+	channelContainer.classList.add('w-full');
+	channelContainer.classList.add('min-h-1/7');
+	channelContainer.classList.add('border-t-1');
+	channelContainer.classList.add('border-t-[#29374d]');
+	channelContainer.classList.add('border-b-2');
+	channelContainer.classList.add('border-b-[#0e121a]');
+	channelContainer.classList.add('bg-[#1c2534]');
+	channelContainer.classList.add('hover:bg-[#3a6280]');
+	channelContainer.classList.add('channel');
+	return channelContainer;
+};
 
-	const channelBtnLike = document.createElement('button');
-	channelBtnLike.innerText = '♥';
-	channelBtnLike.classList.add('text-[#c0c6c9]');
-	channelBtnLike.dataset.info = JSON.stringify(channel);
-	channelBtnLike.classList.add('text-lg');
-	channelBtnLike.classList.add('w-1/6');
-	channelBtnLike.classList.add('sm:max-w-16');
-	channelBtnLike.classList.add('h-full');
-	channelBtnLike.classList.add('channel__btnLike');
-	channelSection.appendChild(channelBtnLike);
+const buildFavoriteButton = (channel) => {
+	const favoriteButton = document.createElement('button');
+	favoriteButton.innerText = '♥';
+	favoriteButton.classList.add('text-[#c0c6c9]');
+	favoriteButton.dataset.info = JSON.stringify(channel);
+	favoriteButton.classList.add('text-lg');
+	favoriteButton.classList.add('w-1/6');
+	favoriteButton.classList.add('sm:max-w-16');
+	favoriteButton.classList.add('h-full');
+	favoriteButton.classList.add('channel__btnLike');
+	return favoriteButton;
+};
 
-	const channelButtonPlay = document.createElement('button');
-	channelButtonPlay.classList.add('w-5/6');
-	channelButtonPlay.classList.add('h-full');
-	channelButtonPlay.classList.add('p-2');
-	channelButtonPlay.classList.add('flex');
-	channelButtonPlay.classList.add('channel__play');
-	channelButtonPlay.classList.add('items-center');
-	channelButtonPlay.dataset.url = channel.url;
+const buildLoadChannelButton = (channel) => {
+	const loadChannelButton = document.createElement('button');
+	loadChannelButton.classList.add('w-5/6');
+	loadChannelButton.classList.add('h-full');
+	loadChannelButton.classList.add('p-2');
+	loadChannelButton.classList.add('flex');
+	loadChannelButton.classList.add('channel__play');
+	loadChannelButton.classList.add('items-center');
+	loadChannelButton.dataset.url = channel.url;
+	return loadChannelButton;
+};
 
+const buildChannelLogo = (channel) => {
 	const channelLogo = document.createElement('img');
 	channelLogo.classList.add('lazy');
 	channelLogo.classList.add('w-10');
 	channelLogo.classList.add('h-full');
 	channelLogo.src = channel.tvgLogo;
-	channelButtonPlay.appendChild(channelLogo);
+	return channelLogo;
+};
 
+const buildChannelName = (channel) => {
 	const channelName = document.createElement('p');
 	channelName.classList.add('text-[#c0c6c9]');
 	channelName.classList.add('ml-4');
 	channelName.classList.add('text-xs');
 	channelName.innerText = channel.name.trim();
-	channelButtonPlay.appendChild(channelName);
-
-	channelSection.appendChild(channelButtonPlay);
-
-	return channelSection;
+	return channelName;
 };
 
-const loadPlaylistM3U = (url, videoElement) => {
+const loadPlaylistM3U = (url, video) => {
 	hls.loadSource(url);
-	hls.attachMedia(videoElement);
+	hls.attachMedia(video);
 };
 
-const loadVideoYoutube = (url, iframeYoutube) => {
+const loadVideoYoutube = (url, youtubePlayer) => {
 	hls.loadSource('');
 	hls.attachMedia(video);
-	iframeYoutube.src = url + '?autoplay=1';
+	youtubePlayer.src = url + '?autoplay=1';
 };
 
 const showElement = (element) => {
@@ -117,8 +123,7 @@ const clearSrcElement = (element) => {
 
 const isYoutubeUrl = (url) => {
 	const regexYoutube = /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)\b/;
-	const result = url.match(regexYoutube);
-	result ? true : false;
+	return regexYoutube.test(url);
 };
 
 const setTextButton = (button, text) => {
@@ -182,16 +187,62 @@ const showModal = (dialog) => {
 	dialog.showModal();
 };
 
+const hideMessaggesModal = (msgError, msgSuccess) => {
+	msgError.classList.add('hidden');
+	msgSuccess.classList.add('hidden');
+};
+
 const closeModal = (dialog) => {
 	dialog.close();
 };
 
-const buildChannelsList = (channels, section) => {
-	section.innerHTML = '';
-	channels.forEach((channel) => {
-		const channelSection = buildChannelSection(channel);
-		section.appendChild(channelSection);
-	});
+const buildChannelSection = (channel) => {
+	const channelSection = buildChannelContainer();
+	const favoriteButton = buildFavoriteButton(channel);
+	const loadChannelButton = buildLoadChannelButton(channel);
+	const channelLogo = buildChannelLogo(channel);
+	const channelName = buildChannelName(channel);
+	loadChannelButton.appendChild(channelLogo);
+	loadChannelButton.appendChild(channelName);
+	channelSection.appendChild(favoriteButton);
+	channelSection.appendChild(loadChannelButton);
+	return channelSection;
+};
+
+const handlePlayChannel = (
+	event,
+	currentlyPlayingChannelSection,
+	unpaintChannelSection,
+	paintChannelSection,
+	isYoutubeUrl,
+	hideElement,
+	showElement,
+	loadVideoYoutube,
+	clearSrcElement,
+	loadPlaylistM3U,
+	video,
+	youtubePlayer
+) => {
+	const buttonLoadChannel = event.target.closest('.channel__play');
+	if (buttonLoadChannel) {
+		const previousChannelSection = currentlyPlayingChannelSection;
+		previousChannelSection && unpaintChannelSection(previousChannelSection);
+		currentlyPlayingChannelSection = buttonLoadChannel.parentElement;
+		paintChannelSection(currentlyPlayingChannelSection);
+
+		const channelUrl = buttonLoadChannel.dataset.url;
+		if (isYoutubeUrl(channelUrl)) {
+			hideElement(video);
+			showElement(youtubePlayer);
+			loadVideoYoutube(channelUrl, youtubePlayer);
+		} else {
+			hideElement(youtubePlayer);
+			clearSrcElement(youtubePlayer);
+			showElement(video);
+			loadPlaylistM3U(channelUrl, video);
+		}
+	}
+	return currentlyPlayingChannelSection;
 };
 
 //Main
@@ -202,101 +253,95 @@ if (Hls.isSupported()) {
 	const favoriteChannels = getFavoritesChannels();
 	const urlM3U = localStorage.getItem('urlM3U');
 	const btnToggleList = document.getElementById('btnToggleList');
-	const videoElement = document.getElementById('video');
-	const iframeYoutube = document.getElementById('playerYoutube');
-	const btnLoadList = document.getElementById('btnLoadList');
-	const btnCancel = document.getElementById('btnCancelLoad');
-	const addListDialog = document.getElementById('addListDialog');
-	const btnConfirmLoad = document.getElementById('btnConfirmLoad');
+	const video = document.getElementById('video');
+	const youtubePlayer = document.getElementById('youtubePlayer');
+	const loadListButton = document.getElementById('loadListButton');
+	const cancelLoadButton = document.getElementById('cancelLoadButton');
+	const loadListDialog = document.getElementById('loadListDialog');
+	const confirmLoadButton = document.getElementById('confirmLoadButton');
+	const msgError = document.getElementById('loadListDialog__error');
+	const msgSuccess = document.getElementById('loadListDialog__success');
 	let isSectionFavoritesVisible = false;
-	let channelSection = null;
-	let counterElements = 0;
-	const initialElements = 15;
-	const elementsToAdd = 15;
-	let isLoading = false;
+	let currentlyPlayingChannelSection = null;
+	let allChannelsControl = {
+		loadedChannelsCount: 0,
+		initialLoadCount: 15,
+		loadMoreCount: 15,
+		isLoading: false,
+		isCompleteList: false,
+	};
+	let favoritesControl = {
+		loadedChannelsCount: 0,
+		initialLoadCount: 15,
+		loadMoreCount: 15,
+		isLoading: false,
+		isCompleteList: false,
+	};
 
-	btnLoadList.addEventListener('click', showModal(addListDialog));
-	btnCancel.addEventListener('click', closeModal(addListDialog));
+	loadListButton.addEventListener('click', () => {
+		hideMessaggesModal();
+		showModal(loadListDialog);
+	});
+	cancelLoadButton.addEventListener('click', () => closeModal(loadListDialog));
 
-	btnConfirmLoad.addEventListener('click', async () => {
+	confirmLoadButton.addEventListener('click', async () => {
 		const urlM3U = document.getElementById('inputAddURLM3U').value;
 		try {
 			const response = await fetch(urlM3U);
 			if (response.status === 200) {
 				localStorage.setItem('urlM3U', urlM3U);
-				addListDialog.close();
-				renderChannels(localStorage.getItem('urlM3U'));
+				msgSuccess.classList.remove('hidden');
 			}
 		} catch (error) {
-			console.log('La url no responde.', error);
+			msgError.classList.remove('hidden');
 		}
 	});
 
-	const addChannelsSections = (initialIndex, amount, channels) => {
-		for (let i = initialIndex; i < initialIndex + amount; i++) {
+	const addChannelsSections = (
+		initialIndex,
+		amount,
+		channels,
+		buildChannelSection,
+		channelsContainer
+	) => {
+		const arraylength = channels.length;
+		let lastIndex = initialIndex + amount;
+		if (lastIndex >= arraylength) {
+			lastIndex = arraylength - 1;
+		}
+		for (let i = initialIndex; i < lastIndex; i++) {
 			const newChannelSection = buildChannelSection(channels[i]);
-			sectionAllChannels.appendChild(newChannelSection);
+			channelsContainer.appendChild(newChannelSection);
 		}
 		return (initialIndex += amount);
 	};
 
 	if (urlM3U) {
-		getFileM3U(urlM3U).then((fileM3U) => {
+		fetchM3UFile(urlM3U).then((fileM3U) => {
 			const channels = getChannels(fileM3U);
-			counterElements = addChannelsSections(
-				counterElements,
-				initialElements,
-				channels
+			allChannelsControl.loadedChannelsCount = addChannelsSections(
+				allChannelsControl.loadedChannelsCount,
+				allChannelsControl.initialLoadCount,
+				channels,
+				buildChannelSection,
+				sectionAllChannels
 			);
 
-			if (favoriteChannels) {
-				buildChannelsList(favoriteChannels, sectionFavorites);
-			}
-
 			sectionAllChannels.addEventListener('click', (event) => {
-				const buttonLoadChannel = event.target.closest('.channel__play');
-				if (buttonLoadChannel) {
-					const previousChannelSection = channelSection;
-					previousChannelSection &&
-						unpaintChannelSection(previousChannelSection);
-					const channelSectionActive = buttonLoadChannel.parentElement;
-					paintChannelSection(channelSectionActive);
-					channelSection = channelSectionActive;
-					const channelUrl = buttonLoadChannel.dataset.url;
-					if (isYoutubeUrl(channelUrl)) {
-						hideElement(videoElement);
-						showElement(iframeYoutube);
-						loadVideoYoutube(channelUrl, iframeYoutube);
-					} else {
-						hideElement(iframeYoutube);
-						clearSrcElement(iframeYoutube);
-						showElement(videoElement);
-						loadPlaylistM3U(channelUrl, videoElement);
-					}
-				}
-			});
-
-			sectionFavorites.addEventListener('click', (event) => {
-				const buttonLoadChannel = event.target.closest('.channel__play');
-				if (buttonLoadChannel) {
-					const previousChannelSection = channelSection;
-					previousChannelSection &&
-						unpaintChannelSection(previousChannelSection);
-					const channelSectionActive = buttonLoadChannel.parentElement;
-					paintChannelSection(channelSectionActive);
-					channelSection = channelSectionActive;
-					const channelUrl = buttonLoadChannel.dataset.url;
-					if (isYoutubeUrl(channelUrl)) {
-						hideElement(videoElement);
-						showElement(iframeYoutube);
-						loadVideoYoutube(channelUrl, iframeYoutube);
-					} else {
-						hideElement(iframeYoutube);
-						clearSrcElement(iframeYoutube);
-						showElement(videoElement);
-						loadPlaylistM3U(channelUrl, videoElement);
-					}
-				}
+				currentlyPlayingChannelSection = handlePlayChannel(
+					event,
+					currentlyPlayingChannelSection,
+					unpaintChannelSection,
+					paintChannelSection,
+					isYoutubeUrl,
+					hideElement,
+					showElement,
+					loadVideoYoutube,
+					clearSrcElement,
+					loadPlaylistM3U,
+					video,
+					youtubePlayer
+				);
 			});
 
 			sectionAllChannels.addEventListener('scroll', () => {
@@ -304,21 +349,71 @@ if (Hls.isSupported()) {
 				const scrollHeight = sectionAllChannels.scrollHeight;
 				const clientHeight = sectionAllChannels.clientHeight;
 
-				if (scrollTop + clientHeight >= scrollHeight - 20 && !isLoading) {
-					isLoading = true;
-					counterElements = addChannelsSections(
-						counterElements,
-						elementsToAdd,
-						channels
+				if (
+					scrollTop + clientHeight >= scrollHeight - 20 &&
+					!allChannelsControl.isLoading &&
+					!allChannelsControl.isCompleteList
+				) {
+					allChannelsControl.isLoading = true;
+					allChannelsControl.loadedChannelsCount = addChannelsSections(
+						allChannelsControl.loadedChannelsCount,
+						allChannelsControl.loadMoreCount,
+						channels,
+						buildChannelSection,
+						sectionAllChannels
 					);
-					isLoading = false;
+					allChannelsControl.isLoading = false;
 				}
 			});
+		});
+	}
 
-			const likeButtons = [...document.querySelectorAll('.channel__btnLike')];
-			likeButtons.forEach((likeButton) => {
-				setupButtonFav(likeButton);
-			});
+	if (favoriteChannels) {
+		favoritesControl.loadedChannelsCount = addChannelsSections(
+			favoritesControl.loadedChannelsCount,
+			favoritesControl.initialLoadCount,
+			favoriteChannels,
+			buildChannelSection,
+			sectionFavorites
+		);
+
+		sectionFavorites.addEventListener('click', (event) => {
+			currentlyPlayingChannelSection = handlePlayChannel(
+				event,
+				currentlyPlayingChannelSection,
+				unpaintChannelSection,
+				paintChannelSection,
+				isYoutubeUrl,
+				hideElement,
+				showElement,
+				loadVideoYoutube,
+				clearSrcElement,
+				loadPlaylistM3U,
+				video,
+				youtubePlayer
+			);
+		});
+
+		sectionFavorites.addEventListener('scroll', () => {
+			const scrollTop = sectionFavorites.scrollTop;
+			const scrollHeight = sectionFavorites.scrollHeight;
+			const clientHeight = sectionFavorites.clientHeight;
+
+			if (
+				scrollTop + clientHeight >= scrollHeight - 20 &&
+				!favoritesControl.isLoading &&
+				!favoritesControl.isCompleteList
+			) {
+				favoritesControl.isLoading = true;
+				favoritesControl.loadedChannelsCount = addChannelsSections(
+					favoritesControl.loadedChannelsCount,
+					favoritesControl.loadMoreCount,
+					favoriteChannels,
+					buildChannelSection,
+					sectionFavorites
+				);
+				favoritesControl.isLoading = false;
+			}
 		});
 	}
 
@@ -333,6 +428,11 @@ if (Hls.isSupported()) {
 			showElement(sectionAllChannels);
 			setTextButton(btnToggleList, 'Favoritos');
 		}
+	});
+
+	const likeButtons = [...document.querySelectorAll('.channel__btnLike')];
+	likeButtons.forEach((likeButton) => {
+		setupButtonFav(likeButton);
 	});
 
 	video.addEventListener('loadeddata', () => {
