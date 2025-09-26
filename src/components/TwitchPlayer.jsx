4b2289d778ff8ada6b5509b66141737c7813df25
Marco Romero <marcod.romero@gmail.com>
2025-09-26 18:26:57 -0300

    Quitar console.log y barra de la url de twitch

--- src/components/TwitchPlayer.jsx
+++ src/components/TwitchPlayer.jsx
@@ -1,8 +1,7 @@
 export default function TwitchPlayer({ channel }) {
-	console.log('recibido', channel);
 	return (
 		<iframe
-			src={`https://player.twitch.tv/?channel=${channel}&parent=zapping-sooty.vercel.app/&muted=false`}
+			src={`https://player.twitch.tv/?channel=${channel}&parent=zapping-sooty.vercel.app&muted=false`}
 			allowfullscreen
 			className="h-full w-full"
 		></iframe>
