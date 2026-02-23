05a5b8ed1ebcb73ea4fa968a9c65afed9304def1
Marco Romero <46422135+marcodromero@users.noreply.github.com>
2026-02-23 00:27:42 -0300

    Hacer que el componente Toolbar reciba a los componentes hijos por children

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,25 @@ import Modal from './features/components/playlistManager';
 import useInitialization from './hooks/useInitialization';
 import Loading from './components/Loading';
 import AppLayout from './components/AppLayout';
+import Search from './features/components/toolbar/components/Search';
+import ToolbarButton from './features/components/toolbar/components/ToolbarButton';
+import { usePlaylistManagerStore } from './store/playlistManagerStore';
 
 function App() {
   const isReady = useInitialization();
+  const openPlaylistManager = usePlaylistManagerStore(
+    (state) => state.openPlaylistManager,
+  );
 
   if (!isReady) <Loading />;
 
   return (
     <AppLayout>
       <Player />
-      <Toolbar />
+      <Toolbar>
+        <Search />
+        <ToolbarButton text='PLAYLISTS' onClick={openPlaylistManager} />
+      </Toolbar>
       <ChannelGuide />
       <Modal />
     </AppLayout>
--- src/features/components/toolbar/index.tsx
+++ src/features/components/toolbar/index.tsx
@@ -1,16 +1,10 @@
-import { usePlaylistManagerStore } from '../../../store/playlistManagerStore';
-import Search from './components/Search';
-import ToolbarButton from './components/ToolbarButton';
+import type { PropsWithChildren } from 'react';
 
-export default function Toolbar() {
-  const openPlaylistManager = usePlaylistManagerStore(
-    (state) => state.openPlaylistManager,
-  );
+export default function Toolbar({ children }: PropsWithChildren) {
   return (
     <section className='w-full flex-1 flex-col min-h-1/10'>
       <div className='flex justify-around items-center p-4 bg-[#1d1e22] text-sm border-b-1 border-b-[#121414] border-t border-t-[#222626] h-[100%] '>
-        <Search />
-        <ToolbarButton text='PLAYLISTS' onClick={openPlaylistManager} />
+        {children}
       </div>
     </section>
   );
